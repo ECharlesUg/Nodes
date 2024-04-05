d0d942e432402a614ecd396f56ee8cc5bae21528
temjfake@gmail.com <temjfake@gmail.com>
2024-04-05 15:18:58 -0500

    Add project files.

---
+++ Nodes/Node.cs
@@ -0,0 +1,31 @@
+﻿namespace Nodes
+{
+    internal class Node
+    {
+        private int _data;
+        private Node? _next;
+        public int Data
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+        public Node? Next
+        {
+            get { return _next; }
+            set { _next = value; }
+        }
+
+        public Node (int value)
+        {
+            Data = value;
+            Next = null;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Data}]";
+        }
+
+        
+    }
+}
---
+++ Nodes/Program.cs
@@ -0,0 +1,32 @@
+﻿using Nodes;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Node node1 = new Node(15);
+        Node node2 = new Node(39);
+        Node node3 = new Node(27);
+        Node node4 = new Node(-10);
+
+        node4.Next = node1;
+        node1.Next = node3;
+        node3.Next = node2;
+
+
+        Nodes(node4);
+
+        static void Nodes(Node node)
+        {
+            while (node != null)
+            {
+                Console.Write(node);
+                node = node.Next;
+                if (node != null)
+                    Console.Write(" -> ");
+            }
+            Console.Write(" -> ");
+            Console.Write("Null");
+        }
+    }
+}
